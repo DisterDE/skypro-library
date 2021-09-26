77834b562fd38ecbb7fd5dc5e3b3d62748684953
DisterRU <disterko@gmail.com>
2021-09-26 17:15:03 +0300

    remove useless annotations

--- src/main/java/pro/sky/java/web/library/entity/Author.java
+++ src/main/java/pro/sky/java/web/library/entity/Author.java
@@ -12,11 +12,9 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name")
     @Length(min = 2, message = "Too short first name")
     private String firstName;
 
-    @Column(name = "last_name")
     @Length(min = 2, message = "Too short last name")
     private String lastName;
 
